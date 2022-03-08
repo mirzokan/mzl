5d4e33d1a69ddc7b20039d9e9de050a227a3bc25
Mirzo Kanoatov <mirzok@gmail.com>
2022-03-08 11:12:46 -0500

    Added list_files_by_ext and copy_files

--- gen.py
+++ gen.py
@@ -4,6 +4,7 @@ General Purpose Tools
 
 import re
 import os
+import shutil
 
 
 def sdir(obj, sunder=False):
@@ -41,7 +42,8 @@ def sdir(obj, sunder=False):
 
 
 def subl(a, b):
-    """Subtract list b from list a.
+    """
+    Subtract list b from list a.
     
     Args:
         a (list): List from which to subtract
@@ -54,6 +56,24 @@ def subl(a, b):
 
 
 def get_latest_file(path, pattern=None):
+    """
+    From a specific folder, get a list of files, optionally matching a
+    regex pattern, and return the last file in alphabetical order.
+    Useful for getting the latest file from a series that contain
+    timestamps in the filename.
+
+    
+    Args:
+        path (str): Path to the folder to scan.
+        pattern (str, optional): Optional regex pattern to use as
+        a filter
+    
+    Returns:
+        TYPE: str
+    
+    Raises:
+        FileNotFoundError: When no matching files are found.
+    """
     files = next(os.walk(path))[2]
     if pattern is not None:
         files = [file for file in files 
@@ -64,3 +84,49 @@ def get_latest_file(path, pattern=None):
     else:
         raise FileNotFoundError
     return latest_file
+
+
+def list_files_by_ext(path, ext, excluded_subfolder_terms=None):
+    """
+    From a specific root folder, get a list of files that have a 
+    specified file extension in the subfolder structure, filtered by 
+    exclusion terms.
+    
+    Args:
+        path (str): Path to the folder to scan.
+        ext (str): File extension to filter for.
+        excluded_subfolder_terms (list): List of terms contained in the 
+                                         folder paths to exclude
+    
+    Returns:
+        TYPE: list
+    """
+    file_list = []
+    
+    for root, dirs, files in os.walk(path):
+        if excluded_subfolder_terms is not None:
+            excluded_subfolder_terms = [item.lower() 
+                                        for item in excluded_subfolder_terms]
+            exclusion_filters = [exclusion in root.lower()
+                                 for exclusion in excluded_subfolder_terms]
+            if any(exclusion_filters):
+                continue
+        for file in files:
+            filepath = os.path.join(root, file)
+            if ((os.path.splitext(filepath)[1] == ext) 
+               and ("~" not in filepath)):
+                file_list.append(filepath)
+    return file_list
+
+
+def copy_files(file_list, destination_path, message=False):
+    for i, file_path in enumerate(file_list):
+        file_name = os.path.basename(file_path)
+        new_path = os.path.join(destination_path, file_name)
+        if os.path.isfile(new_path):
+            if message:
+                print(f"{i}. Exists: {os.path.basename(file_name)}")
+        else:
+            shutil.copy(file_path, new_path)
+            if message:
+                print(f"{i}. Copied: {os.path.basename(file_name)}")
