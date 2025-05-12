682d5746b86277dd8991de810609007fcb84836f
Mirzo Kanoatov <mirzok@gmail.com>
2025-05-11 21:21:37 -0400

    Sci: added type hints, improved docstrings

--- sci.py
+++ sci.py
@@ -1,5 +1,5 @@
 '''
-Routines associated with handling scientific data
+Tools for working with scientific/clinical data
 '''
 
 import re
@@ -7,31 +7,46 @@ import pandas as pd
 import numpy as np
 from IPython.display import display
 import mzl
+from pandas import DataFrame
+from matplotlib.axes import Axes
+from seaborn.axisgrid import FacetGrid
+from typing import Optional, List
+
 
 ##############################
 # LIMS
 ##############################
 
-
-def read_lims(path, sheet_name=None, subject_col = 'patient_id',
-              collection = ['subject', 'visit']):
-    '''
-    Read and pre-process a LIMS file into a pandas Dataframe
+def read_lims(path: str,
+              sheet_name: Optional[str] = None,
+              subject_col: str = 'patient_id',
+              collection: List[str] = ['subject', 'visit']) -> DataFrame:
+    """
+    Load and process a LIMS export file into a standardized pandas DataFrame.
 
     Args:
-        path: Filepath to the LIMS file
-    '''
+        path (str): Filepath to the LIMS exporty (Excel) file.
+        sheet_name (Optional[str]): Name of the sheet to load. Loads the first 
+                                    sheet if None.
+        subject_col (str): Column name to treat as subject identifier.
+        collection (List[str]): Columns to concatenate into a composite
+                                collection identifier.
+
+    Returns:
+        pd.DataFrame: Preprocessed LIMS data with standardized column 
+                      names and date formatting.
+    """
     if sheet_name is not None:
         df = pd.read_excel(path, sheet_name=sheet_name, dtype=object)
     else:
         df = pd.read_excel(path, dtype=object)
 
     df = df.mzl.clean_colnames()
-    rename_cols_dict = {'sample_#': 'limsid', 'project_#': 'project', 
+    rename_cols_dict = {'sample_#': 'limsid', 'project_#': 'project',
                         'sample_id': 'barcode', subject_col: 'subject',
                         'visit_code': 'visit', 
                         'sample_collection_timestamp': 'coldt'}
-
+    
     df = df.rename(rename_cols_dict, axis=1)
 
     date_cols = ['coldt', 'received_on:']
@@ -39,23 +54,38 @@ def read_lims(path, sheet_name=None, subject_col = 'patient_id',
         df[col] = pd.to_datetime(df[col])
 
     df = df.mzl.concat_cols('collection', collection)
-
     return df
 
 
-def get_visit_order(lims):
-    visit_order = (lims[['subject', 'visit', 'coldt']].drop_duplicates()
-                   .sort_values(['subject', 'coldt']).copy())
+def add_visit_order(lims: DataFrame,
+                    preview: bool = False) -> DataFrame:
+    """
+    Imply a visit order based on collection timestamps from multiple subjects. 
+
+    Args:
+        lims (pd.DataFrame): Preprocessed LIMS dataframe.
+
+    Returns:
+        pd.DataFrame: LIMS dataframe with an additional `ordered_visit` column.
+    """
+    visit_order = (lims[['subject', 'visit', 'coldt']]
+                   .drop_duplicates()
+                   .sort_values(['subject', 'coldt'])
+                   .copy())
     visit_order = visit_order.drop_duplicates(['subject', 'visit'],
                                               keep='last')
-
     visit_order['count'] = visit_order.groupby(['subject']).cumcount()
 
-    visit_order = (visit_order[['visit', 'count']].groupby('visit').mean()
-                   .sort_values("count").round(1)
-                   .rename({"count": 'order'}, axis=1).astype(str))
+    visit_order = (visit_order[['visit', 'count']]
+                   .groupby('visit')
+                   .mean()
+                   .sort_values("count")
+                   .round(1)
+                   .rename({"count": 'order'}, axis=1)
+                   .astype(str))
     visit_order['order'] = visit_order['order'].str.zfill(5)
-    display(visit_order)
+    if preview:
+        display(visit_order)
 
     lims = lims.merge(visit_order, how='left', on='visit')
     lims['ordered_visit'] = lims.order + "_" + lims.visit
@@ -66,42 +96,48 @@ def get_visit_order(lims):
 # MyAssay
 ##############################
 
-def read_mad(path):
-    '''
-    Read and pre-process a MAD file into a pandas Dataframe
+def read_mad(path: str) -> DataFrame:
+    """
+    Load and preprocess a MyAssay Desktop (MAD) result file into a 
+    pandas DataFrame.
 
     Args:
-        path: Filepath to the MAD file
-    '''
+        path (str): File path to the MAD data file.
+
+    Returns:
+        pd.DataFrame: Cleaned and formatted MAD data including numeric
+        conversion and annotations.
+    """
     df = pd.read_csv(path, delimiter='\t', dtype=object)
     df = df.mzl.clean_colnames()
-    df = df.rename({'sample': 'limsid', '%cv': 'cv', 
-                    'hemoglobin_mg/dl': 'hemoglobin',
-                    'reported_value': 'result'}, axis=1)
+    df = df.rename({
+        'sample': 'limsid', '%cv': 'cv',
+        'hemoglobin_mg/dl': 'hemoglobin',
+        'reported_value': 'result'
+    }, axis=1)
 
     df = df.loc[df.result != "Not Analyzed"]
 
     numeric_cols = ['dil_factor', 'mean_final_conc', 'sd', 'cv',
                     'lloq', 'uloq', 'reportable_range_low',
                     'reportable_range_high', 'hemoglobin']
-    
+
     df['limsid'] = df.limsid.str.strip()
     df[numeric_cols] = df[numeric_cols].apply(pd.to_numeric, errors='ignore')
     df['result_numeric'] = pd.to_numeric(df.result, errors='coerce')
 
     df['result_filled'] = df.result
     df['is_filled'] = False
-    df.loc[df.result.isin(['< LLOQ', '> ULOQ', 'Invalid']), "is_filled"] = True 
+    df.loc[df.result.isin(['< LLOQ', '> ULOQ', 'Invalid']), 'is_filled'] = True
     df.loc[df.result.isin(['Invalid', '> ULOQ']),
            'result_filled'] = df.mean_final_conc
     df.loc[df.result_filled == "< LLOQ",
            'result_filled'] = df.reportable_range_low
-    df['result_filled'] = pd.to_numeric(df.result_filled, errors='coerce')
+    df['result_filled'] = pd.to_numeric(df['result_filled'], errors='coerce')
 
     df['run_timestamp'] = df['run_timestamp'].str.replace(
-                         r"(\d{2}/\d{2}/\d{4} \d{2}:\d{2}:\d{2}) (?:.{1,10})",
-                         r'\1', regex=True)
-
+        r"(\d{2}/\d{2}/\d{4} \d{2}:\d{2}:\d{2}) (?:.{1,10})",
+        r'\1', regex=True)
     df['run_timestamp'] = pd.to_datetime(df['run_timestamp'])
 
     try:
@@ -120,18 +156,35 @@ def read_mad(path):
     except:
         df['plate'] = np.NaN
 
+
     return df
 
 
-def mad_add_loqs(df, g, uloq=True, lloq=True, log=True, rotate=0):
-    loqs = df.groupby('assay').agg({'reportable_range_high': 'min',
-                                    'reportable_range_low': 'max'})
+def mad_add_loqs(df: DataFrame, g: FacetGrid,
+                 uloq: bool = True, lloq: bool = True,
+                 log: bool = True, rotate: int = 0) -> None:
+    """
+    Overlay assay limits of quantification (LLOQ and ULOQ) on MAD plots.
+
+    Args:
+        df (pd.DataFrame): Assay results including reportable ranges.
+        g (seaborn.FacetGrid): Seaborn grid object containing the plots.
+        uloq (bool): Whether to draw ULOQ lines.
+        lloq (bool): Whether to draw LLOQ lines.
+        log (bool): Whether to apply log scale on y-axis.
+        rotate (int): Angle for x-axis labels rotation.
+
+    Returns:
+        None
+    """
+    loqs = df.groupby('assay').agg({
+        'reportable_range_high': 'min',
+        'reportable_range_low': 'max'})
+
     axes = g.axes.flatten()
-    
     if rotate != 0:
         g.set_xticklabels(rotation=rotate)
         g.fig.tight_layout()
-    
     if log:
         for ax in g.fig.axes:
             ax.set_yscale('log')
@@ -163,14 +216,21 @@ def mad_add_loqs(df, g, uloq=True, lloq=True, log=True, rotate=0):
 # Olink
 ##############################
 
-def read_olink(path, npx=True, delimiter=","):
-    '''
-    Read and pre-process an Olink file into a pandas Dataframe
+def read_olink(path: str,
+               npx: bool = True,
+               delimiter: str = ",") -> DataFrame:
+    """
+    Load and process an Olink result CSV file into a pandas DataFrame.
 
     Args:
-        path: Filepath to the Olink file
-    '''
+        path (str): File path to the Olink CSV export file.
+        npx (bool): Whether to use NPX values
+                    (if False, use quantified values).
+        delimiter (str): Delimiter used in the CSV file.
 
+    Returns:
+        pd.DataFrame: Standardized Olink assay results.
+    """
     df = pd.read_csv(path, delimiter=delimiter, dtype=object)
     df = df.mzl.clean_colnames()
 
@@ -215,18 +275,31 @@ def read_olink(path, npx=True, delimiter=","):
     return df
 
 
-def olink_add_loqs(df, g, uloq=True, lloq=True, log=True, rotate=0, npx=True):
+def olink_add_loqs(df: DataFrame, g: FacetGrid,
+                   uloq: bool = True, lloq: bool = True,
+                   log: bool = True, rotate: int = 0,
+                   npx: bool = True) -> None:
+    """
+    Args:
+        df (pd.DataFrame): Olink data frame containing assay information.
+        g (seaborn.FacetGrid): Grid of seaborn plots.
+        uloq (bool): Display ULOQ line.
+        lloq (bool): Display LLOQ (or LOD for NPX) line.
+        log (bool): Use logarithmic scale for y-axis.
+        rotate (int): Rotate x-axis labels.
+        npx (bool): Indicates if NPX values are used.
+
+    Returns:
+        None
+    """
     if npx:
         loqs = df.groupby('assay').agg({'maxlod': 'max'})
     else:
         loqs = df.groupby('assay').agg({'platelql': 'max', 'uloq': 'min'})
-
     axes = g.axes.flatten()
-    
     if rotate != 0:
         g.set_xticklabels(rotation=rotate)
         g.fig.tight_layout()
-    
     if log:
         for ax in g.fig.axes:
             ax.set_yscale('log')
